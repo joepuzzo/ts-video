b36edfabc7e173f677e5980cc9963b6350a875e4
joepuzzo <jgpuzzo2@gmail.com>
2023-02-15 12:29:40 -0800

    added base

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router>
+    <Router basename="/ts-video">
       <div id="portal-root"></div>
       <App />
     </Router>
